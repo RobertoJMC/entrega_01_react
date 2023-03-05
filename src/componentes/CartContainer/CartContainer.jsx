f8dace53b0ac92881a4fa925edf494b917dd08af
Roberto Millan <millan.roberto1@gmail.com>
2023-03-04 23:50:45 -0300

    intento de devolver ID de orders

--- src/componentes/CartContainer/CartContainer.jsx
+++ src/componentes/CartContainer/CartContainer.jsx
@@ -1,13 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { useCartContext } from '../../context/CartContext'
 import './CartContainer.css'
 import trash from '../../assets/red-trash.png'
+import { addDoc, collection, getDocs, getFirestore } from 'firebase/firestore'
 
 const CartContainer = () => {
 
+  const mostrarId = () => {
+    const [mid,setMid] = useState([])
+    const db = getFirestore() 
+    const queryCollections = collection(db, 'orders')
+    getDocs(queryCollections)
+    .then(resp => setMid( resp.docs.map(product => ({ id: product.id, ...product.data()} ) )))
+    .catch(err => console.error(err))
+    alert(mid.id)
+  }
+
+
+  const [formData, setFormData] = useState( {
+    name: '',
+    phone: '',
+    email:'',
+    repetirEmail: ''
+} )
+
   const {cartList, vaciarCarrito, precioTotal, eliminarProducto} = useCartContext()
-  
+
+  const insertarOrder = (evt) => {
+    evt.preventDefault()
+    const order = {}
+    order.buyer = formData
+
+    const db = getFirestore()
+    const ordersCollection = collection(db, 'orders')
+    
+    addDoc(ordersCollection, order)
+    .then(resp => console.log(resp))
+    .catch(err => console.log(err))
+    .finally(() => {
+        vaciarCarrito()
+        setFormData({
+            name: '',
+            phone: '',
+            email:'',
+            repetirEmail: ''
+        })
+      })
+  }
+
+  const handleOnChange = (evt) => {
+    setFormData({
+        ...formData,
+        [evt.target.name]: evt.target.value
+    })
+}
+
   return (
     <>
     <div className='cabeceraCarrito'>
@@ -17,6 +65,43 @@ const CartContainer = () => {
             ):
             <p className='borde'>{precioTotal() !== 0 && `Total de la compra: ${precioTotal()}`}</p>
             }
+        </div>
+        <div>
+            <form onSubmit={insertarOrder} >
+                <input 
+                    type="text" 
+                    name="name"          
+                    placeholder = "Ingrese el nombre"   
+                    onChange={handleOnChange} 
+                    value={formData.name}
+
+                /><br />
+                <input 
+                    type="text" 
+                    name="phone"         
+                    placeholder = "Ingrese el teléfono" 
+                    onChange={handleOnChange} 
+                    value={formData.phone}
+
+                /><br />
+                <input 
+                    type="text" 
+                    name="email"         
+                    placeholder = "Ingrese el email"    
+                    onChange={handleOnChange} 
+                    value={formData.email}
+
+                /><br />
+                <input 
+                    type="text" 
+                    name="repetirEmail"  
+                    placeholder = "Repetir el email"    
+                    onChange={handleOnChange} 
+                    value={formData.repetirEmail}
+
+                /><br />
+                <button className="btn btn-outline-success" type="submit" onClick={mostrarId}>Generar orden</button>
+            </form>
         </div>
         <div>
             <button class="btn btn-outline-dark" onClick={vaciarCarrito}>Vaciar Carrito</button>
